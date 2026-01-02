a39adbf1ed467946096ee9915e5d91d90a6d0f7c
BelischAseva31 <135888317+BelischAseva31@users.noreply.github.com>
2026-01-02 14:25:12 +0300

    Add deposit and withdraw subcommands to kart
    Added new subcommands for depositing and withdrawing money.

--- Commands/Ekonomi Sistemi/Kart/kart.js
+++ Commands/Ekonomi Sistemi/Kart/kart.js
@@ -13,58 +13,13 @@ module.exports = {
     .addSubcommand((option) => option
       .setName('sil')
       .setDescription('Kredi Kartınızı Silersiniz'))
+    // --- YENİ EKLENENLER ---
+    .addSubcommand((option) => option
+      .setName('yatır')
+      .setDescription('Cüzdanınızdaki parayı bankaya yatırırsınız')
+      .addIntegerOption(opt => opt.setName('miktar').setDescription('Yatırılacak miktar').setRequired(true)))
+    .addSubcommand((option) => option
+      .setName('çek')
+      .setDescription('Bankadaki parayı cüzdanınıza çekersiniz')
+      .addIntegerOption(opt => opt.setName('miktar').setDescription('Çekilecek miktar').setRequired(true)))
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// YouTube: @MZRDev tarafından yapılmıştır. Satılması, paylaşılması tamamen yasaktır!
